ddba25ec834c39e48032bda3ac8d159d20a315a0
ikeda-toshiaki-oec <ikeda_toshiaki@edu.kimura.ac.jp>
2026-02-06 14:59:28 +0900

    update avg

--- src/App.java
+++ src/App.java
@@ -23,4 +23,12 @@ public class App {
     public static int div(int a,int b){
         return 0;
     }
+
+    public static int avg(int a,int b){
+        return (a+b)/2;
+    }
+
+    public static int max(int a,int b){
+        return 0;
+    }
 }
